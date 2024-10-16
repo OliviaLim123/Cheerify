c9b86dcb4d00bc814d8b5d6eb49a7354d6e70cea
OliviaLim123 <131115382+OliviaLim123@users.noreply.github.com>
2024-10-16 13:34:03 +1100

    Create the Xcode Project

---
+++ Cheerify/Cheerify/CheerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerifyApp.swift
+//  Cheerify
+//
+//  Created by Olivia Gita Amanda Lim on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CheerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
