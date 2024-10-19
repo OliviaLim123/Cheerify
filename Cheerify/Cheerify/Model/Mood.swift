1224d63eff7e1ce1211db4f0dffb765c6655b5a2
Shin-nie <thuhangvu0302@gmail.com>
2024-10-19 13:01:39 +1100

    File Updated

---
+++ Cheerify/Cheerify/Model/Mood.swift
@@ -0,0 +1,8 @@
+//
+//  Mood.swift
+//  Cheerify
+//
+//  Created by Hang Vu on 19/10/2024.
+//
+
+import Foundation
---
+++ Cheerify/Cheerify/View/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Cheerify
+//
+//  Created by Hang Vu on 19/10/2024.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
